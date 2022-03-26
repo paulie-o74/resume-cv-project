a2d4a2032ca0fd2a5b0a3d7866d0a1f137d00542
rashdogg74 <pauloriordan74@gmail.com>
2022-03-26 18:32:30 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,14 +1,14 @@
 function userInformationHTML(user) {
     return `
     <h2>${user.name}
-    <span class= "small-name">
-    (@<a href= "${user.html_url}" target="_blank">${user.login}</a>)
+    <span class="small-name">
+    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
     </span>
     </h2>
-    <div class= "gh-content">
+    <div class="gh-content">
     <div class="gh-avatar">
     <a href="${user.html_url}" target="_blank">
-    <img src="${user.avatar_url}" width="80" height="80" alt= "${user.login}"/></a>
+    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" /></a>
     </div>
     <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>`;
@@ -54,7 +54,7 @@ function fetchGitHubInformation(event) {
     );
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function (firstResponse, secondResponse) {
